a7f41bf4f3162666675ab56e2c73a3cd7b94112b
rupeshjha001 <rupesh@gmail.com>
2024-07-26 12:35:41 +0530

    first

--- createfile.py
+++ createfile.py
@@ -1,2 +1,6 @@
 def create_file():
-    pass
+    name=input("enter your file name wich you want to create")+".txt"
+    file=open("d:"+name,w)
+    file.write("hello"+name)
+    file.close
+    print("file has been created")
--- renamefile.py
+++ renamefile.py
@@ -1,2 +1,12 @@
 def rename_file():
-    pass
+    directory = "e:"  # Define the directory where the files are located
+
+    name = input("Enter the file name: ") + ".txt"
+    if name in os.listdir(directory):
+        name2 = input("Enter the new file name: ") + ".txt"
+        os.rename(os.path.join(directory, name), os.path.join(directory, name2))
+        print("Your file name has been changed")
+    else:
+        print("File not found")
+
+rename()
