ed86fc87806049450a639542250f06fe0a3755dd
anamika <anuu.0704@gmail.com>
2024-07-27 16:06:30 +0530

    first

--- removefile.py
+++ removefile.py
@@ -1,2 +1,17 @@
+
+
+
+
+import os 
 def remove_file():
-    pass
+ filename=input("enter file name:")+'.txt'
+ 
+ if(filename in os.listdir('data/')):
+    os.remove('data/'+filename)
+    print("file has been deleted")
+ else:
+    print("file not exist")
+ 
+remove_file()
+
+
